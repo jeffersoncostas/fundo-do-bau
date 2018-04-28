b105ee6c03b5d3c0b303b826ab1a27bc923d8188
Jefferson Costa <jeffersoncosta@outlook.com>
2018-04-28 00:43:56 -0300

    adding style guide page and tabs

---
+++ src/pages/style-guide/style-guide.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the StyleGuidePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage({
+
+})
+@Component({
+  selector: 'page-style-guide',
+  templateUrl: 'style-guide.html',
+})
+export class StyleGuidePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad StyleGuidePage');
+  }
+
+}
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,17 +3,23 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { StyleGuidePage } from '../style-guide/style-guide'
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  about = AboutPage;
+  home = 'HomePage';
+  contact = 'ContactPage';
+  tab4 = 'StyleGuidePage'
 
   constructor() {
+
+  }
+
+  click(): void {
 
   }
 }
