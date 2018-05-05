d6bbfd98d4da4973367dd1ffc5be0768750c4f04
Jefferson Costa <jeffersoncosta@outlook.com>
2018-05-05 19:50:29 -0300

    fdba 101 header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,12 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { StyleGuidePageModule } from '../pages/style-guide/style-guide.module';
-import { ComponentsModule } from '../components/components.module';
 
 import { MyApp } from './app.component';
 import { TabsPage } from '../pages/tabs/tabs';
-import { StyleGuidePage } from '../pages/style-guide/style-guide';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { TesteDesafioDistanciaPageModule } from '../pages/teste-desafio-distancia/teste-desafio-distancia.module';
-import { TesteDesafioDistanciaPage } from '../pages/teste-desafio-distancia/teste-desafio-distancia';
 
 @NgModule({
   declarations: [
@@ -22,7 +17,6 @@ import { TesteDesafioDistanciaPage } from '../pages/teste-desafio-distancia/test
   imports: [
     BrowserModule,
     CommonModule,
-    ComponentsModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
